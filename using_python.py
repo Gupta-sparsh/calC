3c512a8ff996eb84ecc4d65f695e14567dfa1690
Sparsh Gupta <57013208+piratesparsh@users.noreply.github.com>
2020-09-09 19:46:43 +0530

    Update using_python.py
    More functions Added

--- using_python.py
+++ using_python.py
@@ -3,7 +3,6 @@ root = Tk()
 root.title("calC")
 
 
-
 inp = Entry(root,width=45,borderwidth=3)
 #inp.grid(row=0,column=0,columnspan=3,padx=3,pady=3)
 inp.place(x=0,y=0,width=300,height=25)
@@ -14,14 +13,45 @@ def key_pressed(num):
 	inp.insert(0,str(last_entry)+ str(num))
 
 def fun_add():
-	last_entry = inp.get()
 	try:
-		eval(last_entry)
-	except:
-		print("Error")
+		expression=eval(str(inp.get()))
+	except(SyntaxError):
+		inp.delete(0,END)
+		inp.insert(0,str(last_entry)+"+")
 	else:
-    	inp.delete(0,END)
-		inp.insert(0,eval(str(last_entry)))
+		inp.delete(0,END)
+		inp.insert(0,str(expression)+"+")
+
+def fun_subtract():
+	try:
+		expression=eval(str(inp.get()))
+	except(SyntaxError):
+		inp.delete(0,END)
+		inp.insert(0,str(last_entry)+"-")
+	else:
+		inp.delete(0,END)
+		inp.insert(0,str(expression)+"-")
+
+def fun_multiply():
+	try:
+		expression=eval(str(inp.get()))
+	except(SyntaxError):
+		inp.delete(0,END)
+		inp.insert(0,str(last_entry)+"*")
+	else:
+		inp.delete(0,END)
+		inp.insert(0,str(expression)+"*")
+
+def fun_divide():
+	try:
+		expression=eval(str(inp.get()))
+	except(SyntaxError):
+		inp.delete(0,END)
+		inp.insert(0,str(last_entry)+"/")
+	else:
+		inp.delete(0,END)
+		inp.insert(0,str(expression)+"/")
+
 	
 ##########
 
@@ -56,16 +86,26 @@ b0 = Button(root, text="0", width=10, height=2,bg="black",font=("Helvetica",15),
 b0.config(command=lambda:key_pressed(0))
 
 b_multiply =  Button(root, text="*", width=10, height=2,bg="black",font=("Helvetica",15),fg="white", relief="flat")
+b_multiply.config(command=fun_multiply)
+
 b_subtract =  Button(root, text="-", width=10, height=2,bg="black",font=("Helvetica",15),fg="white", relief="flat")
+b_subtract.config(command=fun_subtract)
+
 b_division =  Button(root, text="/", width=10, height=2,bg="black",font=("Helvetica",15),fg="white", relief="flat")
-b_equal =  Button(root, text="=", width=10, height=2,bg="black",font=("Helvetica",15),fg="white", relief="flat")
+b_division.config(command=fun_divide)
 
 b_add = Button(root, text="+", width=10, height=2,bg="black",font=("Helvetica",15),fg="white", relief="flat")
-b_add.config(command=lambda:fun_add()
+b_add.config(command=fun_add)
+
+b_equal =  Button(root, text="=", width=10, height=2,bg="black",font=("Helvetica",15),fg="white", relief="flat")
 
 b_period = Button(root, text=".", width=10, height=2,bg="black",font=("Helvetica",15),fg="white", relief="flat")
+
 b_clear = Button(root,text="Clear")
+b_clear.config(command= lambda : inp.delete((END-1),END))
+
 b_reset = Button(root, text="CE", width=10, height=2,bg="black",font=("Helvetica",15),fg="white", relief="flat")
+b_reset.config(command=lambda : inp.delete(0,END))
 
 
 ###########
